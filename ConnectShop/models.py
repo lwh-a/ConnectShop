b58a5f34e45a154c13efae976fb3a20c43e4bb5d
sdhuen01-del <sdhuen01@gmail.com>
2026-04-13 17:34:40 +0900

    20260413

--- ConnectShop/models.py
+++ ConnectShop/models.py
@@ -1,12 +1,17 @@
 from ConnectShop import db
-
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True , nullable=False)
     password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), unique=True , nullable=False)
-    phone = db.Column(db.String(11), nullable=False)
+    phone = db.Column(db.String(15), nullable=False)
+    is_membership = db.Column(db.Boolean, default=False)
+    join_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
 
 # 임시로 정의한 상품 모델
 class Product(db.Model):
